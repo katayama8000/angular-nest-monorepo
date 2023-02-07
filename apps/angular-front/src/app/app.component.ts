cf33f9240d29dbbbb9104a7e5172f6244c8d1b0c
katayama8000 <tattu.0310@gmail.com>
2023-02-07 12:02:33 +0900

    feat: lint-fix

--- apps/angular-front/src/app/app.component.ts
+++ apps/angular-front/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-front';
-  response = fetch('http://localhost:3333/api').then(res => res.json());
+  response = fetch('http://localhost:3333/api').then((res) => res.json());
 }
