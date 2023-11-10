6e951b23fd496420df718addbffbce4f3c8781b6
katayama8000 <tattu.0310@gmail.com>
2023-11-10 19:57:31 +0900

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
